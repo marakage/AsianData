886392dcd30ecb0397796347faad298fdb438508
Mara <kagem2@uw.edu>
2017-10-27 13:21:47 -0700

    Creating Project

---
+++ Datacleaning.R
@@ -0,0 +1,18 @@
+# Loading packages
+library(readxl)
+# install.packages("tidyverse")
+library(tidyverse)
+
+# reading in dataset
+AsianAlone <- read_csv("../Datasets/Asian_alone/DEC_10_SF2_PCT1_with_ann.csv")
+Cambodian <- read_csv("../Datasets/Cambodian_alone/DEC_10_SF2_PCT1_with_ann.csv")
+Chinese <- read_csv("../Datasets/Chinese/DEC_10_SF2_PCT1_with_ann.csv")
+Filipino <- read_csv("../Datasets/Filipino/DEC_10_SF2_PCT1_with_ann.csv")
+Hmong <- read_csv("../Datasets/Hmong/DEC_10_SF2_PCT1_with_ann.csv")
+Japanese <- read_csv("../Datasets/Japanese/DEC_10_SF2_PCT1_with_ann.csv")
+Korean <- read_csv("../Datasets/Korean/DEC_10_SF2_PCT1_with_ann.csv")
+Laotian <- read_csv("../Datasets/Laotian/DEC_10_SF2_PCT1_with_ann.csv")
+Taiwanese <- read_csv("../Datasets/Taiwanese/DEC_10_SF2_PCT1_with_ann.csv")
+Vietnamese <- read_csv("../Datasets/Vietnamese/DEC_10_SF2_PCT1_with_ann.csv")
+Education <- read_csv("../Datasets/Education/ACS_15_5YR_S1501_with_ann.csv")
+
